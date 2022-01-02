ccc9dd47e466b01eed03943ff2a723c5b462f00a
Jignesh Thummar <jignesh.be.ce@gmail.com>
2022-01-02 14:31:43 -0500

    add components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,151 +1,302 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+    
+    <v-app id="inspire">
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+        <v-navigation-drawer v-model="drawer"
+                             app dark>
+            <!--  -->
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
+            <v-card>
+                <v-list>
+                    <v-list-group v-for="item in navItems"
+                                  :key="item.title"
+                                  v-model="item.active"
+                                  :prepend-icon="item.action"
+                                  no-action>
+                        <template v-slot:activator>
+                            <v-list-item-content>
+                                <v-list-item-title v-text="item.title"></v-list-item-title>
+                            </v-list-item-content>
+                        </template>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+                        <v-list-item v-for="child in item.items"
+                                     :key="child.title">
+                            <v-list-item-content>
+                                <v-list-item-title v-text="child.title"></v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-group>
+                </v-list>
+            </v-card>
+        </v-navigation-drawer>
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
+        <v-app-bar elevation="2" app dark>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
+            <v-toolbar-title>Application</v-toolbar-title>
 
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
+        </v-app-bar>
+        
+        <v-main>
+            <template>
+                <v-progress-linear value="15"></v-progress-linear>
+            </template>
+            <template>
+                <v-card>
+                    <v-container>
+                        <v-row>
+                            <v-col>
+                                <v-tabs v-model="tab"
+                                        dark>
+                                    <v-tab v-for="item in tabItems"
+                                           :key="item.tab">
+                                        {{ item.tab }}
+                                    </v-tab>
+                                </v-tabs>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
+                                <v-tabs-items v-model="tab">
+                                    <v-tab-item v-for="item in tabItems"
+                                                :key="item.tab">
+                                        <v-card flat>
+                                            <template>
+                                                <v-card>
 
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+                                                    
+
+                                                    <v-list three-line>
+                                                        
+                                                        <template v-for="(item, index) in tabContentItems">
+
+                                                            <v-subheader v-if="item.header"
+                                                                         :key="item.header"
+                                                                         v-text="item.header"></v-subheader>
+
+                                                            <v-divider v-else-if="item.divider"
+                                                                       :key="index"
+                                                                       :inset="item.inset"></v-divider>
+
+                                                            <v-list-item v-else
+                                                                         :key="item.title">
+                                                                <v-list-item-avatar>
+                                                                    <v-img :src="item.avatar"></v-img>
+                                                                </v-list-item-avatar>
+
+                                                                <v-list-item-content>
+                                                                    <v-list-item-title v-html="item.title"></v-list-item-title>
+                                                                    <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                                                                </v-list-item-content>
+                                                            </v-list-item>
+
+                                                        </template>
+                                                    </v-list>
+
+                                                </v-card>
+                                            </template>
+                                        </v-card>
+                                    </v-tab-item>
+                                </v-tabs-items>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card>
+            </template>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+    export default {
+        data: () => ({
+            drawer: null,
+            navItems: [
+                {
+                    action: 'mdi-ticket',
+                    items: [{ title: 'List Item' }],
+                    title: 'Attractions',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    active: true,
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining',
+                },
+                {
+                    action: 'mdi-school',
+                    items: [{ title: 'List Item' }],
+                    title: 'Education',
+                },
+                {
+                    action: 'mdi-human-male-female-child',
+                    items: [{ title: 'List Item' }],
+                    title: 'Family',
+                },
+                {
+                    action: 'mdi-bottle-tonic-plus',
+                    items: [{ title: 'List Item' }],
+                    title: 'Health',
+                },
+                {
+                    action: 'mdi-briefcase',
+                    items: [{ title: 'List Item' }],
+                    title: 'Office',
+                },
+                {
+                    action: 'mdi-tag',
+                    items: [{ title: 'List Item' }],
+                    title: 'Promotions',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining1',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining2',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining3',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining4',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining5',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining6',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining7',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining8',
+                },
+                {
+                    action: 'mdi-silverware-fork-knife',
+                    items: [
+                        { title: 'Breakfast & brunch' },
+                        { title: 'New American' },
+                        { title: 'Sushi' },
+                    ],
+                    title: 'Dining9',
+                }
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-  }
+            ],
+
+            tab: null,
+            tabItems: [
+                { tab: 'One', content: 'Tab 1 Content' },
+                { tab: 'Two', content: 'Tab 2 Content' },
+                { tab: 'Three', content: 'Tab 3 Content' },
+                { tab: 'Four', content: 'Tab 4 Content' },
+                { tab: 'Five', content: 'Tab 5 Content' },
+                { tab: 'Six', content: 'Tab 6 Content' },
+                { tab: 'Seven', content: 'Tab 7 Content' },
+                { tab: 'Eight', content: 'Tab 8 Content' },
+                { tab: 'Nine', content: 'Tab 9 Content' },
+                { tab: 'Ten', content: 'Tab 10 Content' },
+            ],
+
+            tabContentItems: [
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                    title: 'Brunch this weekend?',
+                    subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+                },
+                { divider: true, inset: true },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                    title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+                    subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+                },
+                { divider: true, inset: true },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                    title: 'Oui oui',
+                    subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+                },
+                { divider: true, inset: true },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                    title: 'Birthday gift',
+                    subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+                },
+                { divider: true, inset: true },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                    title: 'Recipe to try',
+                    subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+                },
+                { divider: true, inset: true },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                    title: 'Recipe to try 1',
+                    subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+                },
+                { divider: true, inset: true },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                    title: 'Recipe to try 2',
+                    subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+                },
+                { divider: true, inset: true },
+                {
+                    avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+                    title: 'Recipe to try 3',
+                    subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+                },
+            ],
+
+        }),
+    }
 </script>
